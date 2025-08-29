d29f7df4d32609aa0f25c109adb3ce8ec6211cc6
liusj5257 <liusj5257@gmail.com>
2025-08-29 15:50:30 +0800

    更新应用宝

--- sh/patch_yingyongbao.sh
+++ sh/patch_yingyongbao.sh
@@ -8,8 +8,7 @@ download_azurlane () {
     # 使用curl命令下载apk文件
     #curl -o blhx.apk  $url
     #url="https://c1.g.mi.com/package/AppStore/01d47c0d09ac743e3906d18e7269f4b786a951c12/eyJhcGt2Ijo2MjIwLCJuYW1lIjoiY29tLmJpbGliaWxpLmJsaHgubWkiLCJ2ZXJzaW9uIjoiMS4wIiwiY2lkIjoibWVuZ18xNDM5XzM0NV9hbmRyb2lkIiwibWQ1IjpmYWxzZX0/5d79bb51f9146ab21cdbc5621c2a85d5"
-    url="https://dd.myapp.com/sjy.00005/sjy.00002/16891/apk/D2355F01029F0A9F586652074EA83319.apk?fsname=com.tencent.tmgp.bilibili.blhx_9.5.10.apk"
-    #curl -o blhx.apk -L $url
+    url="https://imtt2.dd.qq.com/sjy.00008/sjy.00002/16891/apk/9903C0B0EB455A130EC7B802EB38F346.apk?fsname=com.tencent.tmgp.bilibili.blhx_9611.apk"
     axel -n 16 -k -o blhx.apk $url
     fi
 }
@@ -22,7 +21,7 @@ fi
 
 
 echo "Decompile Azur Lane apk"
-java -jar apktool.jar  -f d AzurLane.apk
+java -jar apktool.jar   d -f AzurLane.apk
 
 echo "Copy libs"
 cp -r libs/. AzurLane/lib/
@@ -55,7 +54,7 @@ sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Dev_Liu\"\n\n\    invoke-sta
 
 
 echo "Build Patched Azur Lane apk"
-java -jar apktool.jar  -f b AzurLane -o AzurLane.patched.apk
+java -jar apktool.jar  b -f AzurLane -o AzurLane.patched.apk
 
 echo "Set Github Release version"
 echo "PERSEUS_VERSION=$(echo YingYongBao)" >> $GITHUB_ENV
